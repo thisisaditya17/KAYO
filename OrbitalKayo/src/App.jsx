4ccb717ca41e7c3c5df6abece059a5a2f4d5ec23
arnav444a <93905434+arnav444a@users.noreply.github.com>
2024-05-30 19:04:24 +0800

    server made + responses set up

--- OrbitalKayo/src/App.jsx
+++ OrbitalKayo/src/App.jsx
@@ -1,10 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Container, Center, Heading, Flex, Stack, Input} from '@chakra-ui/react'
-
+import { Container, Center, Heading, Flex, Stack,Button, Input} from '@chakra-ui/react'
+import axios from 'axios'
 function App() {
+  
   const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null);
+  const [data, setData] = useState('')
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setData(response.data.processed_content);
+      console.log('File uploaded successfully:', response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
 
   return (
 <>
@@ -14,7 +38,9 @@ function App() {
     <Heading>KAYO - AI for the future</Heading>
     <Heading>Know it All Yield Optimizer</Heading>
       <Input type='text' placeholder="Enter Prompt here" size='lg'></Input>
-      <Input type='file' size='sm'></Input>
+      <Input onChange={handleFileChange} type='file' size='sm'></Input>
+      <Button onClick={handleSubmit}>Upload</Button>
+      <Heading>processed content: {data}</Heading>
     </Stack>
     </Flex>
     </Center>
